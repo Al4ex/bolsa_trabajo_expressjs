fc387d0a89942da13a28edf19680ebc28cba290b
Alexander <alexandercruzaragon@gmail.com>
2021-01-19 22:42:48 -0600

    Subir todo1

--- src/index.js
+++ src/index.js
@@ -7,5 +7,4 @@ require('./database');
 // Server is listening
 app.listen(app.get('port'), () => {
   console.log('ingresa a localhost:',app.get('port'));
-  console.log('Environment:', process.env.NODE_ENV);
 });
